6e6fb280a637c520c5d1cf8d718162492684d418
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-18 09:09:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(names,event) {
+<<<<<<< HEAD
   let print=[];
   for (let i = 0; i < names.length; i++) {   
   
@@ -8,6 +9,15 @@ function writeCards(names,event) {
   }
  
   return print;
+=======
+  let result[];
+  for (let i = 0; i < names.length; i++) {   
+  
+     result[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+ 
+  return result;
+>>>>>>> 99463f36f0f107b9ba28480bcea96e20a4b6817f
 }
 
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
